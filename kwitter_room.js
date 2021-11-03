51b77704273f0292ba73b3bd4010b84cc61faf96
Aditya-mehrotra-01 <82381553+Aditya-mehrotra-01@users.noreply.github.com>
2021-11-03 13:46:14 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBEPPM4VUhXBs9kZKF4exGRWeTLsBuN0mU",
+    authDomain: "projectc-94-b5307.firebaseapp.com",
+    databaseURL: "https://projectc-94-b5307-default-rtdb.firebaseio.com",
+    projectId: "projectc-94-b5307",
+    storageBucket: "projectc-94-b5307.appspot.com",
+    messagingSenderId: "13206634809",
+    appId: "1:13206634809:web:c3c9ac4a1bb03e867168e8"
+  };
